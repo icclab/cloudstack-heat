48ca2e5addd80e1b716d25a0c7d8fef2584b75f9
cima <cima@zhaw.ch>
2015-09-18 11:05:02 +0200

    removed unfinished Cloudstack Network resource

--- cloudstack_plugin.py
+++ cloudstack_plugin.py
@@ -132,78 +132,11 @@ class CloudstackVirtualMachine(resource.Resource):
                 return True
 
         return False
-'''
-class CloudstackNetwork(resource.Resource):
-    PROPERTIES = (DISPLAY_TEXT, NAME, NETWORK_OFFERING_ID, ZONE_ID) = \
-        ('display_text', 'name', 'network_offering_id', 'zone_id')
-
-    properties_schema = {
-        DISPLAY_TEXT: properties.Schema(
-            properties.Schema.STRING,
-            _('The display text of the network'),
-            True
-        ),
-        NAME: properties.Schema(
-            properties.Schema.STRING,
-            _('The name of the network'),
-            True
-        ),
-        NETWORK_OFFERING_ID: properties.Schema(
-            properties.Schema.STRING,
-            _('Network offering ID'),
-            True
-        ),
-        ZONE_ID: properties.Schema(
-            properties.Schema.STRING,
-            _('Zone ID'),
-            True
-        )
-    }
-
-    def _get_cloudstack(self):
-        cs = CloudStack(endpoint=API_ENDPOINT,
-                        key=API_KEY,
-                        secret=API_SECRET)
-        return cs
-
-    def handle_create(self):
-        cs = self._get_cloudstack()
-
-        displaytext = self.properties.get(self.DISPLAY_TEXT)
-        name = self.properties.get(self.NAME)
-        networkofferingid = self.properties.get(self.NETWORK_OFFERING_ID)
-        zoneid = self.properties.get(self.ZONE_ID)
-
-        network = cs.createNetwork(displaytext=displaytext,
-                                   name=name,
-                                   networkofferingid=networkofferingid
-                                   zoneid=zoneid)
-
-        self.resource_id_set(network.id)
-        return network.id
-
-    def check_create_complete(self, _compute_id):
-        # TODO
-        pass
-
-    def handle_delete(self):
-        cs = self._get_cloudstack()
-
-        if self.resource_id is None:
-            return
-
-        cs.deleteNetwork(self.resource_id)
-
-    def check_delete_complete(self, _compute_id):
-        # TODO
-        pass
-'''
 
 
 def resource_mapping():
     mappings = {}
 
     mappings['Cloudstack::Compute::VirtualMachine'] = CloudstackVirtualMachine
-#    mappings['Cloudstack::Network::Network'] = CloudstackNetwork
 
     return mappings
