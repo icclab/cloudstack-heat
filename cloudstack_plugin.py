9272fd7e500b215f5509e858095956d7476e7d88
cima <cima@zhaw.ch>
2015-10-29 16:02:37 +0100

    [src] added Cloudstack::Network::SecurityGroup resource

--- cloudstack_plugin.py
+++ cloudstack_plugin.py
@@ -1,4 +1,5 @@
 from cs import CloudStack
+from cs import CloudStackException
 
 from heat.engine import properties
 from heat.engine import resource
@@ -109,17 +110,25 @@ class CloudstackVirtualMachine(resource.Resource):
 
         if self.resource_id is None:
             return
-
-        cs.destroyVirtualMachine(id=self.resource_id)
+        try:
+            cs.destroyVirtualMachine(id=self.resource_id)
+        except CloudStackException as e:
+            print e
 
     def check_delete_complete(self, _compute_id):
         cs = self._get_cloudstack()
-
-        vm = cs.listVirtualMachines(id=self.resource_id)
+        vm = None
+        try:
+            vm = cs.listVirtualMachines(id=self.resource_id)
+        except CloudStackException as e:
+            if e.args[2]['errorcode'] == 431:
+                # Resource cannot be found
+                return True
+            print e
         if vm:
             return False
-
-        return True
+        else:
+            return True
 
     def handle_suspend(self):
         cs = self._get_cloudstack()
@@ -171,9 +180,121 @@ class CloudstackVirtualMachine(resource.Resource):
     }
 
 
+class CloudstackSecurityGroup(resource.Resource):
+    PROPERTIES = (
+        API_ENDPOINT,
+        API_KEY,
+        API_SECRET,
+        NAME,
+        RULES) = (
+        'api_endpoint',
+        'api_key',
+        'api_secret',
+        'name',
+        'rules')
+
+    properties_schema = {
+        API_ENDPOINT: properties.Schema(
+            data_type=properties.Schema.STRING,
+            description=_('Cloudstack API endpoint'),
+            required=True
+        ),
+        API_KEY: properties.Schema(
+            data_type=properties.Schema.STRING,
+            description=_('API key'),
+            required=True
+        ),
+        API_SECRET: properties.Schema(
+            data_type=properties.Schema.STRING,
+            description=_('API secret key'),
+            required=True
+        ),
+        NAME: properties.Schema(
+            data_type=properties.Schema.STRING,
+            description=_('The name of the security group'),
+            required=True
+        ),
+        RULES: properties.Schema(
+            data_type=properties.Schema.LIST,
+            description=_('List of ingress / egress rules'),
+            required=False
+        )
+    }
+
+    def _get_cloudstack(self):
+        cs = CloudStack(endpoint=self.properties.get(self.API_ENDPOINT),
+                        key=self.properties.get(self.API_KEY),
+                        secret=self.properties.get(self.API_SECRET))
+        return cs
+
+    def handle_create(self):
+        cs = self._get_cloudstack()
+
+        name = self.properties.get(self.NAME)
+        rules = self.properties.get(self.RULES)
+
+        sg = cs.createSecurityGroup(name=name)
+
+        sg_id = sg['securitygroup']['id']
+
+        for rule in rules:
+            if rule.get('direction', 'ingress') == 'ingress':
+                cs.authorizeSecurityGroupIngress(
+                    securitygroupid=sg_id,
+                    startport=rule.get('startport', None),
+                    endport=rule.get('endport', None),
+                    cidrlist=rule.get('cidr', '0.0.0.0/0'),
+                    protocol=rule.get('protocol', 'tcp'))
+            elif rule.get('direction', 'ingress') == 'egress':
+                cs.authorizeSecurityGroupEgress(
+                    securitygroupid=sg_id,
+                    startport=rule.get('startport', None),
+                    endport=rule.get('endport', None),
+                    cidrlist=rule.get('cidr', '0.0.0.0/0'),
+                    protocol=rule.get('protocol', 'tcp'))
+
+        self.resource_id_set(sg_id)
+        return sg_id
+
+    def check_create_complete(self, _compute_id):
+        cs = self._get_cloudstack()
+
+        sg = cs.listSecurityGroups(id=self.resource_id)
+        if sg:
+            return True
+
+        return False
+
+    def handle_delete(self):
+        cs = self._get_cloudstack()
+
+        if self.resource_id is None:
+            return
+        try:
+            cs.deleteSecurityGroup(id=self.resource_id)
+        except CloudStackException as e:
+            print e
+
+    def check_delete_complete(self, _compute_id):
+        cs = self._get_cloudstack()
+        sg = None
+        try:
+            sg = cs.listSecurityGroups(id=self.resource_id)
+        except CloudStackException as e:
+            if e.args[2]['errorcode'] == 431:
+                # Resource cannot be found
+                return True
+            print e
+        if sg:
+            return False
+        else:
+            return True
+
+
 def resource_mapping():
     mappings = {}
 
     mappings['Cloudstack::Compute::VirtualMachine'] = CloudstackVirtualMachine
+    mappings['Cloudstack::Network::SecurityGroup'] = CloudstackSecurityGroup
 
     return mappings
